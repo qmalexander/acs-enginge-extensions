35777fef3e038c82762c5d90035cc7ac49afd951
Alexander Wahlsten <alexander@qmnet.se>
2018-04-10 10:57:58 +0200

    Update run.ps1
    Added copy item

--- fix_external_communication/v1/run.ps1
+++ fix_external_communication/v1/run.ps1
@@ -1,2 +1,3 @@
 $trigger = New-JobTrigger -AtStartup -RandomDelay 00:00:30
 Register-ScheduledJob -Trigger $trigger -FilePath c:\fix_external_communication.ps1 -Name AcsFixExternal
+Copy-Item "fix_external_communication.ps1" -Destination "c:\"
