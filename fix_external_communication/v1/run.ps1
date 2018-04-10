3dcd593aa4f87a519e34c3ad076032801453abb8
Alexander Wahlsten <alexander@qmnet.se>
2018-04-10 10:56:37 +0200

    Update run.ps1

--- fix_external_communication/v1/run.ps1
+++ fix_external_communication/v1/run.ps1
@@ -1,2 +1,2 @@
 $trigger = New-JobTrigger -AtStartup -RandomDelay 00:00:30
-Register-ScheduledJob -Trigger $trigger -FilePath c:\fix.ps1 -Name AcsFixExternal
+Register-ScheduledJob -Trigger $trigger -FilePath c:\fix_external_communication.ps1 -Name AcsFixExternal
